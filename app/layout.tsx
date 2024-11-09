08fed0018935b3ff1c2e2c62dd0400f3108098d1
Sgt. Cookie <50783352+Sgt-Cook1e@users.noreply.github.com>
2024-11-08 21:17:50 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import "./bootstrap.min.css";
+import "./modesta.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
