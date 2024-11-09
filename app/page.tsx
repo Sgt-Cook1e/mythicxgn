4f3091483a8c34f26eac5aa9e11c7e3927c4a19e
Sgt. Cookie <50783352+Sgt-Cook1e@users.noreply.github.com>
2024-11-08 21:23:57 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,11 @@ function Home() {
                     <p>Page Still Under Development.</p>
                 </div>
                 <div className="row top">
-                    <div className="col my-auto col-lg-5 col-xl-6 col-main">
+                    <div className="">
                         <a href="https://discord.com/users/847363776961314817">
                             <img src="https://lanyard.cnrad.dev/api/847363776961314817?bg=23283d&borderRadius=8px" className="status"></img>
                         </a>
-                        <h4 className="roadrage support">Support My Work!</h4>
-                        <a className="btn btn1 glas button-glas" role="button" href="https://patreon.com/MythicStudios">Patreon</a>
-                        {/* <a className="btn btn1 glas button-glas" role="button" href="https://discord.gg/MeZUrZ6Aa3">Support Discord</a> */}
-                    </div>
+                     </div>
                     <div className="col col-lg-7 col-xl-6 col-box">
                         <div className="container">
                             <a href="https://github.com/Sgt-Cook1e"></a>
